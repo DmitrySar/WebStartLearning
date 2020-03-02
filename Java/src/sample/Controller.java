c2ad15969982d91b8533f346e5aa87c47e9f5ede
DmitrySar <youshin.d@gmail.com>
2020-03-02 14:07:13 +0300

    remove HTML text

--- Java/src/sample/Controller.java
+++ Java/src/sample/Controller.java
@@ -9,6 +9,10 @@ import javafx.scene.web.WebView;
 import org.w3c.dom.Document;
 import org.w3c.dom.html.HTMLInputElement;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Controller {
 
     WebEngine engine;
@@ -55,43 +59,8 @@ public class Controller {
     }
 
     @FXML
-    private void initialize() {
-        editor.setText("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "\t<title>Sample</title>\n" +
-                "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"example.css\">\n" +
-                "</head>\n" +
-                "\n" +
-                "<script type=\"text/javascript\">\n" +
-                "function fill(s) {\n" +
-                "\tdocument.getElementById('sel').style.background = \"red\";\n" +
-                "}\n" +
-                "\n" +
-                "function sum(idA, idB, idC) {\n" +
-                "\ta = document.getElementById(idA).value;\n" +
-                "\tb = document.getElementById(idB).value;\n" +
-                "\tlet c = Number(a) + Number(b);\n" +
-                "\tdocument.getElementById(idC).value = c;\n" +
-                "}\n" +
-                "</script>\n" +
-                "\n" +
-                "<body>\n" +
-                "\t<p id = \"sel\">\n" +
-                "                hhhhhhh\n" +
-                "                </p>\n" +
-                "\t<p>\n" +
-                "\t<button type=\"button\" onclick=\"fill('ssss')\">click</button>\n" +
-                "</p>\n" +
-                "\t\t<p>\n" +
-                "\t\t<input type=\"text\" id=\"a\" value=\"1\">\n" +
-                "                +\n" +
-                "                <input type=\"text\" id=\"b\" value=\"2\">\n" +
-                "\t\t<button onclick=\"sum('a', 'b', 'c')\">=</button>\n" +
-                "\t\t<input type=\"text\" id=\"c\">\n" +
-                "\t</p>\n" +
-                "</body>\n" +
-                "</html>");
+    private void initialize() throws IOException {
+        Files.lines(Paths.get("Java/res/index.html")).forEach(s -> editor.appendText(s + "\n"));
         engine = view.getEngine();
         engine.loadContent(editor.getText());
     }
